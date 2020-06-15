164a1e8fcb298f4fae1a3d46418ecdc63f8d45a3
wenwen <wenwen@edmodo.com>
2020-06-15 21:00:09 +0800

    model for top100_tweet

--- models/top100_tweet.py
+++ models/top100_tweet.py
@@ -7,13 +7,14 @@ from app import db
 class Top100Tweets(db.Model):
 
     __tablename__='top100_tweet'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    tweet_id = db.relationship('world_cup_2018_tweet', backref='tweet', lazy=True)
+    list_id = db.Column(db.Integer, primary_key=True, unique=True)
     hashtag = db.Column(db.String(255), nullable=False)
+    tweet_id =db.relationship('WorldCup2018Tweets', backref='popular_tweet_for_world_cup', lazy='dynamic')
 
-    def __int__(self, tweet_id=None, hashtag=None):
+    def __int__(self, list_id=None, tweet_id=None, hashtag=None):
+        self.popular_list = list_id
         self.tweet_id = tweet_id
         self.hashtag = hashtag
 
     def __repr__(self):
-        return'<Top100Tweets %r>' % self.tweet_id
+        return'<Top100Tweets %r>' % self.popular_list
