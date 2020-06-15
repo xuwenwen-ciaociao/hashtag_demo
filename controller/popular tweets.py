45bb3036a4cca90f1c72181ad67e7bbce407b987
wenwen <wenwen@edmodo.com>
2020-06-15 20:57:03 +0800

    one to one to select data from Mysql

--- controller/popular tweets.py
+++ controller/popular tweets.py
@@ -4,10 +4,10 @@ from app import app, db
 from models.world_cup_2018_tweet import WorldCup2018Tweets
 
 
-@app.route('/hashtag/GetAll', methods={'GET'})
-def get_all_tweets():
-    list = WorldCup2018Tweets.query.all()
-    _list = []
-    for item in list:
-        _list.append(item)
-    return jsonify(_list)
+@app.route('/hashtag/<id>/')
+def get_popular_tweets(id):
+    tweet_id = Top100Tweets.query.get(id)
+    Origin_tweets = WorldCup2018Tweets.query.filter(WorldCup2018Tweets.tweet_id == tweet_id).all()
+    return ','.join(a.Origin_tweets for a in Origin_tweets)
+
+
