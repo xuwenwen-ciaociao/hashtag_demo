a531a3bb5361e73c39ea5e2792e0e07989d5218b
wenwen <wenwen@edmodo.com>
2020-06-19 18:09:51 +0800

    update

--- models/world_cup_2018_tweet.py
+++ models/world_cup_2018_tweet.py
@@ -8,7 +8,7 @@ class WorldCup2018Tweets(db.Model):
 
     __tablename__ = 'world_cup_2018_tweets'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    tweet_id = db.Column(db.String(255), db.ForeignKey('Top100Tweets.tweet_id'), nullable=False)
+    tweet_id = db.Column(db.String(255), db.ForeignKey('top100_tweets.tweet_id'), nullable=True)
     Name = db.Column(db.String(255), nullable=False)
     Orig_tweet = db.Column(db.String(4096), nullable=False)
     RTs = db.Column(db.String(255), nullable=True)
@@ -16,15 +16,9 @@ class WorldCup2018Tweets(db.Model):
     Followers = db.Column(db.String(255), nullable=True)
     Friends = db.Column(db.String(255), nullable=True)
 
-
-    def __int__(self, tweet_id=None, Name=None, Orig_tweet=None, RTs=None, Likes=None, Followers=None, Friends=None):
-        self.tweet_id = ID
-        self.name = Name
-        self.orig_tweet = Orig_tweet
-        self.retweet_count = RTs
-        self.favorite_count = Likes
-        self.followers = Followers
-        self.friends = Friends
-
-    def __repr__(self):
-        return'<RussiaTrollTweet %r>' % self.title
+    # def __int__(self, tweet_id=None, user_id=None, text=None, retweet_count=None, favorite_count=None):
+    #     self.tweet_id = tweet_id
+    #     self.user_id = user_id
+    #     self.orig_tweets = text
+    #     self.retweet_count = retweet_count
+    #     self.favorite_count = favorite_count
