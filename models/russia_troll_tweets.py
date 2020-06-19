0292698709ad581f308fad75641199756667045f
wenwen <wenwen@edmodo.com>
2020-06-19 18:09:28 +0800

    update

--- models/russia_troll_tweets.py
+++ models/russia_troll_tweets.py
@@ -4,18 +4,21 @@ from app import db
 class RussiaTrollTweet(db.Model):
 
     __tablename__ = 'russia_troll_tweets'
-    tweet_id = db.Column(db.String(255), primary_key=True,  nullable=False)
-    user_id = db.relationship('russia_troll_users', secondary='user_tweets', backref='cou')
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    tweet_id = db.Column(db.String(255), db.ForeignKey('top100_tweets.tweet_id'), nullable=True)
+    user_id = db.Column(db.String(4096), nullable=False)
     text = db.Column(db.String(4096), nullable=False)
     retweet_count = db.Column(db.String(255), nullable=False)
     favorite_count = db.Column(db.String(255), nullable=False)
+    
+    
 
-    def __int__(self, tweet_id=None, user_id=None, text=None, retweet_count=None, favorite_count=None):
-        self.tweet_id = tweet_id
-        self.user_id = user_id
-        self.orig_tweets = text
-        self.retweet_count = retweet_count
-        self.favorite_count = favorite_count
-
-    def __repr__(self):
-        return'<RussiaTrollTweet %r>' % self.tweet_id
+    # def __int__(self, tweet_id=None, user_id=None, text=None, retweet_count=None, favorite_count=None):
+    #     self.tweet_id = tweet_id
+    #     self.user_id = user_id
+    #     self.orig_tweets = text
+    #     self.retweet_count = retweet_count
+    #     self.favorite_count = favorite_count
+    #
+    # def __repr__(self):
+    #     return'<RussiaTrollTweet %r>' % self.tweet_id
