ef92aac5fdbc3087cf05bbb122d9b87de7cb0e19
wenwen <wenwen@edmodo.com>
2020-06-19 18:10:05 +0800

    update

--- models/top100_tweets.py
+++ models/top100_tweets.py
@@ -9,5 +9,6 @@ class Top100Tweets(db.Model):
     __tablename__='top100_tweets'
     id = db.Column(db.Integer, primary_key=True, unique=True)
     hashtag = db.Column(db.String(255), nullable=False)
-    tweet = db.relationship('WorldCup2018Tweets', backref='popular_tweet_for_world_cup', uselist=False)
+    tweet_for_world_cup = db.relationship('WorldCup2018Tweets', backref='popular_tweet_for_world_cup', lazy=True)
+    tweet_for_russia_troll = db.relationship('RussiaTrollTweet', backref='popular_tweet_for_russia_troll', lazy=True)
     tweet_id = db.Column(db.String(255), nullable=False)
